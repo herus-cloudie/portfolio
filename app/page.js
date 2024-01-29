0b59fbee5d18996a0da8928020f7de4d3608613c
Amir <herus2006cripto.gmail.com>
2024-01-29 22:59:25 +0330

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Phone from "@/component/module/phone";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsTelegram } from "react-icons/bs";
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container">
+      <aside>
+        <div className="brand">H</div>
+        <div className="brand-small">R</div>
+        <div className="social">
+          <a href="https://linkedin.com/in/amirmohammad-moslemi" className="linkedin"><BsLinkedin /></a>
+          <a href="https://github.com/herus-cloudie" className="github"><BsGithub /></a>
+          <a href="https://t.me/Herus_soll" className="telegram"><BsTelegram /></a>
         </div>
+        <div className="column"/>
+      </aside>
+      <header>
+        <ul>
+          <a href="#" className="li portfolio">portfolio</a>
+          <a href="#" className="li">about me</a>
+          <a href="#" className="li">my blog</a>
+          <a href="#" className="li">reviews</a>
+          <a href="#" className="li">contact me</a>
+        </ul>
+        <a href="#" className="call">
+          <Phone />
+          Schedule a Call
+        </a>
+      </header>
+      <main>
+        <div className="hero-text"> 
+          <h6 className="hithere">Hi There,</h6>
+          <h1 className="main-text">I'm He<p className="redR">r</p>us</h1>
+          <h3 className="main-text-h3">Front-end developer during daytime</h3>
+          <h4 className="main-text-h4">& work with batman during nights.</h4>
+          <button class="button-49" role="button">See Portfolio</button>
+        </div>
+        <img src="/me.png" className="hero-img"/>
+      </main>
+      <div>
+
+        <div className="email">
+          <h5>Email</h5>
+          <p>herus2006crypto@gmail.com</p>
+          <img className="gmail" src="/gmail.png"/>
+        </div>
+        <div className="contact-column"/>
+        <div className="location">
+          <h5>Location</h5>
+          <p>Iran , Tehran</p>
+          <img className="placeholder" src="/placeholder.png"/>
+        </div>
+
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ component/module/portfolioBtn.js
@@ -0,0 +1,5 @@
+export default function PortfolioBtn(){
+    return(
+        <button class="button-49" role="button">Button 49</button>
+    )
+}
